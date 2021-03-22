6ba7fcd539c084b4464022693d0a94db29b7736a
Nikolay0107 <nimphonoe@gmail.com>
2021-03-22 10:51:38 +0200

    Start of development

---
+++ src/main/java/com/abank/task/app/PaymentPortalApplication.java
@@ -0,0 +1,11 @@
+package com.abank.task.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentPortalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentPortalApplication.class, args);
+    }
+}
