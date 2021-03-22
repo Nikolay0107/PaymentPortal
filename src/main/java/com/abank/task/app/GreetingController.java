911e3cd2e20a521939a3407011807d8855c71b7c
Nikolay0107 <nimphonoe@gmail.com>
2021-03-22 14:50:42 +0200

    Added PSQL and filter

--- src/main/java/com/abank/task/app/GreetingController.java
+++ src/main/java/com/abank/task/app/GreetingController.java
@@ -1,24 +1,64 @@
 package com.abank.task.app;
 
+import com.abank.task.app.domain.Message;
+import com.abank.task.app.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class GreetingController {
+    @Autowired
+    private MessageRepo messageRepo;
+
 
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Map<String, Object> model) {
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Map<String, Object> model) {
         model.put("name", name);
         return "greeting";
     }
 
     @GetMapping
-    public String main(Map<String, Object> model){
-        model.put("some", "Hello, lets code!");
+    public String main(Map<String, Object> model) {
+        Iterable<Message> messages = messageRepo.findAll();
+
+        model.put("messages", messages);
         return "main";
     }
+
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+
+        //to save
+        Message message = new Message(text, tag);
+        messageRepo.save(message);
+
+        // to take from repo and give to user
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model){
+        Iterable<Message> messages;
+
+        if(filter!=null && !filter.isEmpty()){
+            messages = messageRepo.findByTag(filter);
+        }else{
+            messages = messageRepo.findAll();
+        }
+
+      model.put("messages", messages);
+        return "main";
+    }
+
+
 }
---
+++ src/main/java/com/abank/task/app/repos/MessageRepo.java
@@ -0,0 +1,10 @@
+package com.abank.task.app.repos;
+
+import com.abank.task.app.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Integer> {
+    List<Message> findByTag(String tag);
+}
